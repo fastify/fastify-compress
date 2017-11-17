b40aa85780f6a39eec834403d173944aeb3dac63
delvedor <tommydelved@gmail.com>
2017-11-17 12:43:03 +0100

    Added onSend hook

--- index.js
+++ index.js
@@ -2,29 +2,40 @@
 
 const fp = require('fastify-plugin')
 const zlib = require('zlib')
-const brotli = require('iltorb')
 const pump = require('pump')
 const sts = require('string-to-stream')
+const mimedb = require('mime-db')
 const supportedEncodings = ['deflate', 'gzip', 'br']
+const compressibleTypes = /^text\/|\+json$|\+text$|\+xml$/
 
 const compressStream = {
   gzip: zlib.createGzip,
-  deflate: zlib.createDeflate,
-  br: brotli.compressStream
+  deflate: zlib.createDeflate
 }
 
 function compressPlugin (fastify, opts, next) {
   fastify.decorateReply('compress', compress)
+  if (opts.global === true) {
+    fastify.addHook('onSend', onSend)
+  }
+  if (opts.brotli) {
+    compressStream.br = opts.brotli.compressStream
+  }
   next()
 }
 
 function compress (payload) {
+  var type = this.res.getHeader('Content-Type') || 'application/json'
+  if (shouldCompress(type) === false) {
+    return this.send(payload)
+  }
+
   if (this.request.headers['x-no-compression'] !== undefined) {
     return this.send(payload)
   }
 
   if (!payload) {
-    this._req.log.warn('compress: missing payload')
+    this.res.log.warn('compress: missing payload')
     this.send(new Error('Internal server error'))
     return
   }
@@ -45,7 +56,7 @@ function compress (payload) {
 
   if (!payload._readableState) {
     if (typeof payload !== 'string') {
-      payload = JSON.stringify(payload)
+      payload = this.serialize(payload)
     }
     payload = sts(payload)
   }
@@ -59,7 +70,54 @@ function compress (payload) {
 }
 
 function onEnd (err) {
-  if (err) this._req.log.error(err)
+  if (err) this.res.log.error(err)
+}
+
+function onSend (req, reply, payload, next) {
+  var type = reply.res.getHeader('Content-Type') || 'application/json'
+  if (shouldCompress(type) === false) {
+    return next()
+  }
+
+  if (req.headers['x-no-compression'] !== undefined) {
+    return next()
+  }
+
+  if (!payload) {
+    reply.res.log.warn('compress: missing payload')
+    next(new Error('Internal server error'))
+    return
+  }
+
+  var encoding = getEncodingHeader(req)
+
+  if (encoding === undefined) {
+    closeStream(payload)
+    reply.code(400)
+    next(new Error('Missing `accept encoding` header'))
+    return
+  }
+
+  if (encoding === null) {
+    closeStream(payload)
+    reply.code(406)
+    next(new Error('Unsupported encoding'))
+    return
+  }
+
+  if (!payload._readableState) {
+    if (typeof payload !== 'string') {
+      payload = reply.serialize(payload)
+    }
+    payload = sts(payload)
+  }
+
+  reply.header('Content-Encoding', encoding)
+  next(null, pump(
+    payload,
+    compressStream[encoding](),
+    onEnd.bind(reply))
+  )
 }
 
 function closeStream (payload) {
@@ -82,6 +140,13 @@ function getEncodingHeader (request) {
     }
   }
   return null
+}
+
+function shouldCompress (type) {
+  if (compressibleTypes.test(type)) return true
+  var data = mimedb[type.split(';', 1)[0].trim().toLowerCase()]
+  if (data === undefined) return false
+  return data.compressible
 }
 
 module.exports = fp(compressPlugin, '>=0.20.0')
