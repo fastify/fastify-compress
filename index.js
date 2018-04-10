61f5845b19306349b70193dbfd4fcb1e4312248f
Jimmy Hurrah <jimmy.hurrah@gmail.com>
2018-04-10 14:21:28 +0200

    cache trimmed encoding

--- index.js
+++ index.js
@@ -132,10 +132,11 @@ function getEncodingHeader (request) {
   if (!header) return undefined
   var acceptEncodings = header.split(',')
   for (var i = 0; i < acceptEncodings.length; i++) {
-    if (supportedEncodings.indexOf(acceptEncodings[i].trim()) > -1) {
-      return acceptEncodings[i].trim()
+    var acceptEncoding = acceptEncodings[i].trim()
+    if (supportedEncodings.indexOf(acceptEncoding) > -1) {
+      return acceptEncoding
     }
-    if (acceptEncodings[i].trim().indexOf('*') > -1) {
+    if (acceptEncoding.indexOf('*') > -1) {
       return 'gzip'
     }
   }
