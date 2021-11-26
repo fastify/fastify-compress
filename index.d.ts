805fb181421c65bc07bfd9da91fd51a9dbb50931
darkgl0w <31093081+darkgl0w@users.noreply.github.com>
2021-11-26 13:41:37 +0100

    types: update typescript types

--- index.d.ts
+++ index.d.ts
@@ -1,31 +1,64 @@
-import { FastifyPlugin, FastifyReply, FastifyRequest } from 'fastify';
-import { Input, InputObject } from 'into-stream';
-import { Stream } from 'stream';
-import { BrotliOptions, ZlibOptions } from 'zlib';
+import * as fastify from 'fastify'
+import { FastifyPluginCallback, FastifyReply, FastifyRequest, RawServerBase, RawServerDefault } from 'fastify'
+import { Input, InputObject } from 'into-stream'
+import { Stream } from 'stream'
+import { BrotliOptions, ZlibOptions } from 'zlib'
 
-declare module "fastify" {
+type EncodingToken = 'br' | 'deflate' | 'gzip' | 'identity';
+
+export interface FastifyCompressOptions {
+  brotliOptions?: BrotliOptions;
+  customTypes?: RegExp;
+  encodings?: EncodingToken[];
+  forceRequestEncoding?: EncodingToken;
+  global?: boolean;
+  inflateIfDeflated?: boolean;
+  onInvalidRequestPayload?: (encoding: string, request: FastifyRequest, error: Error) => Error | undefined | null;
+  onUnsupportedEncoding?: (encoding: string, request: FastifyRequest, reply: FastifyReply) => string | Buffer | Stream;
+  onUnsupportedRequestEncoding?: (encoding: string, request: FastifyRequest, reply: FastifyReply) => Error | undefined | null;
+  requestEncodings?: EncodingToken[];
+  threshold?: number;
+  zlib?: unknown;
+  zlibOptions?: ZlibOptions;
+}
+
+interface RouteCompressOptions extends Pick<FastifyCompressOptions,
+  | 'brotliOptions'
+  | 'customTypes'
+  | 'encodings'
+  | 'inflateIfDeflated'
+  | 'onUnsupportedEncoding'
+  | 'threshold'
+  | 'zlib'
+  | 'zlibOptions'
+> {}
+
+interface RouteDecompressOptions extends Pick<FastifyCompressOptions,
+  | 'forceRequestEncoding'
+  | 'onInvalidRequestPayload'
+  | 'onUnsupportedRequestEncoding'
+  | 'requestEncodings'
+  | 'zlib'
+> {}
+
+interface FastifyCompressRouteOptions {
+  compress?: false | RouteCompressOptions;
+  decompress?: false | RouteDecompressOptions;
+}
+
+export interface RouteOptions extends fastify.RouteOptions, FastifyCompressRouteOptions {}
+
+declare module 'fastify' {
+  export interface RouteShorthandOptions<
+    RawServer extends RawServerBase = RawServerDefault
+  > extends FastifyCompressRouteOptions { }
+
   interface FastifyReply {
     compress(input: Stream | Input | InputObject): void;
   }
+
+  export interface RouteOptions extends FastifyCompressRouteOptions {}
 }
 
-type EncodingToken = 'br' | 'deflate' | 'gzip' | 'identity'
-
-export interface FastifyCompressOptions {
-  global?: boolean
-  threshold?: number
-  customTypes?: RegExp
-  zlib?: NodeModule
-  brotliOptions?: BrotliOptions
-  zlibOptions?: ZlibOptions
-  inflateIfDeflated?: boolean
-  onUnsupportedEncoding?: (encoding: string, request: FastifyRequest, reply: FastifyReply) => string | Buffer | Stream
-  encodings?: Array<EncodingToken>
-  requestEncodings?: Array<EncodingToken> 
-  forceRequestEncoding?: EncodingToken
-  onUnsupportedRequestEncoding?: (encoding: string, request: FastifyRequest, reply: FastifyReply) => Error | undefined | null
-  onInvalidRequestPayload?: (encoding: string, request: FastifyRequest, error: Error) => Error | undefined | null
-}
-
-declare const fastifyCompress: FastifyPlugin<FastifyCompressOptions>
-export default fastifyCompress;
+declare const fastifyCompress: FastifyPluginCallback<FastifyCompressOptions>
+export default fastifyCompress
