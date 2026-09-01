bb726a33749875ef5643db39497145f51cf84d86
lprnmns <manasalperen@gmail.com>
2026-09-01 17:34:30 +0300

    fix: preserve unsupported request encoding value

--- test/global-decompress.test.js
+++ test/global-decompress.test.js
@@ -322,7 +322,7 @@ describe('It should return the error returned by :', async () => {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
-        'content-encoding': 'whatever'
+        'content-encoding': 'WhatEver'
       },
       payload: createPayload(zlib.createDeflate)
     })
@@ -331,7 +331,7 @@ describe('It should return the error returned by :', async () => {
       statusCode: 400,
       code: 'INVALID',
       error: 'Bad Request',
-      message: 'We don\'t want to deal with whatever.'
+      message: 'We don\'t want to deal with WhatEver.'
     })
   })
 
